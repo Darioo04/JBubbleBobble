3146157b18bd1e70d8ae662ab1092e8fc21d2baa
Angelo Meridiani <a.meridi04@gmail.com>
2024-06-17 19:35:07 +0200

    Update and rename Gioca.java to Play.java

---
+++ src/view/Play.java
@@ -0,0 +1,12 @@
+package view;
+import javax.swing.*;
+public class Play extends JFrame {
+	private JFrame play;
+	
+	public Gioca() {
+		play = new JFrame();
+		play.setSize(900,600);
+		play.setVisible(true);
+		play.setLocationRelativeTo(null);
+	}
+}
