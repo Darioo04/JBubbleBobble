1042f325f49044323b09df83b65d0dc24ade07c0
Angelo Meridiani <a.meridi04@gmail.com>
2024-06-17 19:41:42 +0200

    Update Play.java

--- src/view/Play.java
+++ src/view/Play.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Play extends JFrame {
 	private JFrame play;
 	
-	public Gioca() {
+	public Play() {
 		play = new JFrame();
 		play.setSize(900,600);
 		play.setVisible(true);
