e9945e171eb6e9a5904623cef30ed1fba562d7ec
Darioo04 <ojog.dario@gmail.com>
2024-07-27 19:30:31 +0200

    nada, solo pulizia del game panel

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -1,17 +1,13 @@
 package view;
 
-import java.awt.BorderLayout;
+
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.util.Observable;
-import java.util.Observer;
+import java.awt.Dimension;
 
-import javax.swing.*;
 
+import model.GameConstants;
 import model.Player;
 
-
-@SuppressWarnings("deprecation")
 
 public class GamePanel extends StateScreenView {
 	private static GamePanel instance;
@@ -24,50 +20,53 @@ public class GamePanel extends StateScreenView {
     }
 
 	private GamePanel() {
-		super();
-		setVisible(true);
-		setBackground(Color.BLACK);
-		add(new JPanel(new GridLayout(2,1,0,0)) {
-			{
-				add(new JPanel(new GridLayout(2,1,0,0)) {
-					{
-						add(new JPanel(new BorderLayout()) {
-							{
-								setBackground(Color.BLACK);
-								FontView font = new FontView("HP");
-								add(font,BorderLayout.CENTER);
-							}
-						});
-						add(new JPanel() {
-							{
-								setBackground(Color.BLACK);
-								add(new JLabel("0"));
-							}
-						});
-						
-					}
-				},BorderLayout.WEST);
-				add(new JPanel(new GridLayout(2,1,0,0)) {
-					{
-						add(new JPanel(new BorderLayout()) {
-							{
-								setBackground(Color.BLACK);
-								FontView font = new FontView("High score");
-								add(font,BorderLayout.CENTER);
-							}
-						});
-						add(new JPanel() {
-							{
-								setBackground(Color.BLACK);
-								add(new JLabel("0"));
-							}
-						});
-						
-					}
-				},BorderLayout.EAST);
-				
-			}
-		},BorderLayout.PAGE_START);
+		
+		this.setBackground(Color.black);
+		this.setPreferredSize(new Dimension(GameConstants.SCREEN_WIDTH, GameConstants.SCREEN_HEIGHT));
+//		super();
+//		setVisible(true);
+//		setBackground(Color.BLACK);
+//		add(new JPanel(new GridLayout(2,1,0,0)) {
+//			{
+//				add(new JPanel(new GridLayout(2,1,0,0)) {
+//					{
+//						add(new JPanel(new BorderLayout()) {
+//							{
+//								setBackground(Color.BLACK);
+//								FontView font = new FontView("HP");
+//								add(font,BorderLayout.CENTER);
+//							}
+//						});
+//						add(new JPanel() {
+//							{
+//								setBackground(Color.BLACK);
+//								add(new JLabel("0"));
+//							}
+//						});
+//						
+//					}
+//				},BorderLayout.WEST);
+//				add(new JPanel(new GridLayout(2,1,0,0)) {
+//					{
+//						add(new JPanel(new BorderLayout()) {
+//							{
+//								setBackground(Color.BLACK);
+//								FontView font = new FontView("High score");
+//								add(font,BorderLayout.CENTER);
+//							}
+//						});
+//						add(new JPanel() {
+//							{
+//								setBackground(Color.BLACK);
+//								add(new JLabel("0"));
+//							}
+//						});
+//						
+//					}
+//				},BorderLayout.EAST);
+//				
+//			}
+//		},BorderLayout.PAGE_START);
 		
 		
 	}
