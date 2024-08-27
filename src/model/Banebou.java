cfbb0eff700794b76d35a8f0a579392d5c8faf42
Giovanni Scafetta <giovi.scaf@gmail.com>
2024-08-27 21:43:45 +0200

    nemici e sistemazione livelli

--- src/model/Banebou.java
+++ src/model/Banebou.java
@@ -1,20 +1,30 @@
 package model;
 
+import controller.GameController;
+
 @SuppressWarnings("deprecation")
 
 public class Banebou extends Enemy {
 	//si muovono saltando ad arco
 	private static final int JUMP_STRENGHT = 4;
+	private int targetY;
+	
 	public Banebou(int x, int y) {
 		super(x,y);
 		setPath("/sprites/banebou/");
 		setDirection(Direction.RIGHT);
+		setNumIdleSprites(1);
+		setNumRunningSprites(0);
+		setNumJumpingSprites(3);
 		setSpeed(4);
+		
 	}
 	
 	@Override
 	public void update() {
 		super.update();
+		setEnemyCollision();
+		setDirectionToGo();
 		if (Math.random() < 0.03) { // 10% di probabilità di cambiare direzione
             randomizeDirection(); 
         }
@@ -27,7 +37,7 @@ public class Banebou extends Enemy {
 				}
 				else {
 					randomizeDirection();
-				}
+				} 
 			}
 			
 			case LEFT -> {
@@ -41,12 +51,42 @@ public class Banebou extends Enemy {
 			}
 			
 			default ->{}
-		}
+			}
+		updateHitbox();
 		setChanged();
         notifyObservers();
 	}
 	
 	private void randomizeDirection() {
         setDirection((Math.random()<=0.5) ? Direction.LEFT : Direction.RIGHT);
+	}
+	private boolean isNextStepOnTile(boolean right) {
+		if (right == true) {
+			x += this.speed;
+			collisionChecker.checkTileCollision(this);
+			x -= this.speed;
+			if (!collisionDown) return false;
+			else return true;
+		} else {
+			x -= this.speed;
+            collisionChecker.checkTileCollision(this);
+            x += this.speed;
+            if (!collisionDown) return false;
+            else return true;
+		}
+	}
+	private void setDirectionToGo() {
+		if (collisionLeft) {
+			setDirection(Direction.RIGHT);
+		}
+		else if (collisionRight) {
+            setDirection(Direction.LEFT);
+        }
+	}
+	private void setEnemyCollision() {
+		collisionLeft = false;
+		collisionRight = false;
+		collisionDown = false;
+		collisionChecker.checkTileCollision(this);
 	}
 }
